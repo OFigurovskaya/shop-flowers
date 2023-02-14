d3293dfb96829a4c97ee2a450bbf65b108aee481
Olesya Figurovskaya <lisonkadrug@gmail.com>
2023-02-14 18:31:46 +0300

    initial

---
+++ vue-project/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  name: 'App',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    Header
+  }
+}
+</script>
+
+<template>
+  <Header />
+</template>
